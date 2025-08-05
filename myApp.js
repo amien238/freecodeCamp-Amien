f105a62a08baa3d3340bcf5e8a5267ed06c92ce9
amien238 <amien23804@gmail.com>
2025-08-05 16:43:40 +0700

    use helmet.frameguard

--- myApp.js
+++ myApp.js
@@ -1,8 +1,8 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
 
 
-const helmet = require('helmet');
 app.use(helmet.hidePoweredBy()); //Nếu không sử dụng dòng code này, web sẽ hiển thị thông tin về Express.js trong tiêu đề HTTP. x-powered-by="Express"
 app.use(helmet.frameguard({action: 'deny'})); // Ngăn chặn clickjacking bằng cách không cho phép trang web được nhúng trong iframe
 
