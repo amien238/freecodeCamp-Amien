45ce70a631fc04c3b8df5104e55fbb735e343667
amien238 <amien23804@gmail.com>
2025-08-05 19:49:59 +0700

    use helmet.frameguard2

--- myApp.js
+++ myApp.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const helmet = require('helmet');
+const api = require('./server.js');
+
 const app = express();
 
 
-// app.use(helmet.hidePoweredBy()); //Nếu không sử dụng dòng code này, web sẽ hiển thị thông tin về Express.js trong tiêu đề HTTP. x-powered-by="Express"
-app.use(helmet.frameguard({action: 'deny'})); // Ngăn chặn clickjacking bằng cách không cho phép trang web được nhúng trong iframe
+app.use(helmet.hidePoweredBy()); //Nếu không sử dụng dòng code này, web sẽ hiển thị thông tin về Express.js trong tiêu đề HTTP. x-powered-by="Express"
+app.use(helmet.frameguard({ action: 'deny' })); // Ngăn chặn clickjacking bằng cách không cho phép trang web được nhúng trong iframe
 
 
 app.get('/', (req, res) => {
@@ -16,18 +18,22 @@ app.get('/', (req, res) => {
 
 
 
-module.exports = app;
-
-const api = require('./server.js');
 app.use(express.static('public'));
 app.disable('strict-transport-security');
 app.use('/_api', api);
+
+
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-let port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
-});
+
+module.exports = app;
+
+if (require.main === module) {
+  let port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`Your app is listening on port ${port}`);
+  });
+}
 
 
