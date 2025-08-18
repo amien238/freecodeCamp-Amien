63f866f4c1e1ddde9050a4c33bd992ab1d332d79
amien238 <amien23804@gmail.com>
2025-08-18 14:13:14 +0700

    test

--- myApp.js
+++ myApp.js
@@ -4,12 +4,6 @@ const helmet = require('helmet');
 const app = express();
 
 
-app.use(helmet.frameguard({ action: 'deny' })); // Ngăn chặn clickjacking bằng cách không cho phép trang web được nhúng trong iframe
-
-
-app.get('/', (req, res) => {
-  res.send('Hello, security!');
-});
 
 
 
@@ -24,6 +18,16 @@ app.use('/_api', api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
+
+
+
+app.use(helmet.frameguard({ action: 'deny' })); // Ngăn chặn clickjacking bằng cách không cho phép trang web được nhúng trong iframe
+
+
+app.get('/', (req, res) => {
+  res.send('Hello, security!');
+});
+
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
