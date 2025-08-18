fb7c18a96423dcb212f82a429f374899bc9c22e7
amien238 <amien23804@gmail.com>
2025-08-18 14:38:33 +0700

    test

--- myApp.js
+++ myApp.js
@@ -50,14 +50,14 @@ app.use(helmet.frameguard({ action: 'deny' }));
 
 
 module.exports = app;
-const api = require('./server.js');
-app.use(express.static('public'));
-app.disable('strict-transport-security');
-app.use('/_api', api);
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
-let port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
-});
+// const api = require('./server.js');
+// app.use(express.static('public'));
+// app.disable('strict-transport-security');
+// app.use('/_api', api);
+// app.get("/", function (request, response) {
+//   response.sendFile(__dirname + '/views/index.html');
+// });
+// let port = process.env.PORT || 3000;
+// app.listen(port, () => {
+//   console.log(`Your app is listening on port ${port}`);
+// });
