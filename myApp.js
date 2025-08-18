32725ed9a048d125494f2cde6908817a187dda1e
amien238 <amien23804@gmail.com>
2025-08-18 15:18:40 +0700

    test

--- myApp.js
+++ myApp.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(helmet());
 app.use(helmet.hidePoweredBy());
 // app.use(helmet.frameguard({ action: 'deny' }));
-app.use(helmet.xssFilter());
+app.use(helmet.xssFilter({}));
 // app.use(helmet.noSniff());
 // app.use(helmet.ieNoOpen());
 
