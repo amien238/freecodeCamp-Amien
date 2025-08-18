19574515bd776d672e6c2428edf0dcafefd7c304
amien238 <amien23804@gmail.com>
2025-08-18 14:06:12 +0700

    test

--- myApp.js
+++ myApp.js
@@ -18,22 +18,17 @@ app.get('/', (req, res) => {
 
 
 
+module.exports = app;
+const api = require('./server.js');
 app.use(express.static('public'));
 app.disable('strict-transport-security');
 app.use('/_api', api);
-
-
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-
-module.exports = app;
-
-if (require.main === module) {
-  let port = process.env.PORT || 3000;
-  app.listen(port, () => {
-    console.log(`Your app is listening on port ${port}`);
-  });
-}
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Your app is listening on port ${port}`);
+});
 
 
